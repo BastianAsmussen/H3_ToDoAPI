262a7db5f774eade1454062df0634edee1136848
Bastian Asmussen <bastian@asmussen.tech>
2024-02-02 13:41:35 +0100

    Added `HelloWorld` component.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,36 @@
+use yew::{function_component, Html, html, use_state};
+
+#[function_component]
+fn HelloWorld() -> Html {
+    html! {
+        <div>
+            <h1>{ "Hello, World!" }</h1>
+        </div>
+    }
+}
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <HelloWorld />
+
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
